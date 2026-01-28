0c80a8e2429547e0fbbedb15665ecf23a38403ee
Mihály Orsós <ormraat.pte@gmail.com>
2026-01-28 22:33:14 +0100

    timer

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/timer_goldhand_space/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
